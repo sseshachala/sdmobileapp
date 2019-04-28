d9d7ba846dff448d2bdce78073a47ee50021c2ac
Sudhi <sudhi@b2bsphere.com>
2019-04-28 08:04:05 -0500

    Refactored the REST API and also introuced version for the api.
    search cosolidated.

--- app/Http/Controllers/sdmobileapp/SearchController.php
+++ app/Http/Controllers/sdmobileapp/SearchController.php
@@ -42,4 +42,36 @@ class SearchController extends Controller
         }
         */
     }
+
+    public function doSearch1(Request $request) {
+
+        //print_r($request); die();
+        $searchTerm = $request->searchTerm;
+
+
+        $header = array (
+            'Content-Type' => 'application/json; charset=UTF-8',
+            'charset' => 'utf-8'
+        );
+
+        $results = MilletDietForDisease::filter(filter_var($searchTerm, FILTER_SANITIZE_STRING));
+
+        if(empty($results) || count($results) > 0)
+        {
+            return response()->json(MilletDietForCancer::filter($searchTerm, 200, $header, JSON_UNESCAPED_UNICODE));
+        }
+        else
+        {
+            return response()->json($results, 200, $header, JSON_UNESCAPED_UNICODE);
+        }
+
+        /*
+         *
+         switch ($filterType)
+        {
+            case 'ailment_or_disease': return response()->json(MilletDietForDisease::filter(filter_var($searchTerm, FILTER_SANITIZE_STRING)), 200, $header, JSON_UNESCAPED_UNICODE); break;
+            case 'cancer_type': return response()->json(MilletDietForCancer::filter(filter_var($searchTerm, FILTER_SANITIZE_STRING)), 200, $header, JSON_UNESCAPED_UNICODE); break;
+        }
+        */
+    }
 }
