70cbe6b778aaed939f89405fce7770d8bd571ea1
Sudhi <sudhi@b2bsphere.com>
2019-04-28 18:44:40 -0500

    Simplified the disease tables.

--- app/Http/Controllers/sdmobileapp/MilletAvailabilityLocationController.php
+++ app/Http/Controllers/sdmobileapp/MilletAvailabilityLocationController.php
@@ -12,14 +12,41 @@ class MilletAvailabilityLocationController extends Controller
     public function showAll()
     {
         $rows = MilletAvailabilityLocation::paginate(15);
+
         $arr = [];
+        /*
+         *
+         * {"current_page":1,"data":[{"id":1,"name":"Organic Sphere","contact_name":"Sudhi Seshachala",
+         * "contact_phone":"4082039960","address":"24910, Kuykendahl Road, Suite H, Tomball, TX 77375",
+         * "address1":"<div class=\"mapouter\"><div class=\"gmap_canvas\"><iframe width=\"600\" height=\"500\" id=\"gmap_canvas\" src=\"https:\/\/maps.google.com\/maps?q=24910%2C+Kuykendahl+Road%2C+Suite+H%2C+Tomball%2C+TX+77375&t=&z=13&ie=UTF8&iwloc=&output=embed\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\"><\/iframe><a href=\"https:\/\/www.emojilib.com\">emojilib.com<\/a><\/div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}<\/style><\/div>"}],
+         * "first_page_url":"http:\/\/192.168.64.2\/sdmobileapp\/public\/sdmobile\/api\/v1.0\/milletLocations?page=1",
+         * "from":1,"last_page":1,"last_page_url":"http:\/\/192.168.64.2\/sdmobileapp\/public\/sdmobile\/api\/v1.0\/milletLocations?page=1",
+         * "next_page_url":null,"path":"http:\/\/192.168.64.2\/sdmobileapp\/public\/sdmobile\/api\/v1.0\/milletLocations",
+         * "per_page":15,"prev_page_url":null,"to":1,"total":1}
+         *
+         */
+        $arr['current_page'] = $rows['current_page'];
+        $arr['first_page_url'] = $rows['first_page_url'];
+        $arr['from'] = $rows['from'];
+        $arr['last_page'] = $rows['last_page'];
+        $arr['last_page_url'] = $rows['last_page_url'];
+        $arr['per_page'] = $rows['per_page'];
+        $arr['prev_page_url'] = $rows['prev_page_url'];
+        $arr['to'] = $rows['to'];
+        $arr['total'] = $rows['total'];
+
         foreach($rows as $row)
         {
-            $row->address = self::geocode($row->address);
+            $obj = new \stdClass();
 
-            $arr[] = $row;
+            $obj->	description = '<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q='.urlencode($row->address).'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.emojilib.com">emojilib.com</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>';
+            $obj->	name = $row->	name . ' '.$row->contact_name.'<br>'.$row->contact_phone;
+
+
+            $arr['data'][] =  $obj;
 
         }
+
         return response()->json($arr);
     }
 
