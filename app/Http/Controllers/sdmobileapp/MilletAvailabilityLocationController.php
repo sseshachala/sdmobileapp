c2e9db73b1d475c9a887f088dd11753dbcdb156b
Sudhi <sudhi@b2bsphere.com>
2019-04-29 12:34:01 -0500

    Simplified the disease tables.

--- app/Http/Controllers/sdmobileapp/MilletAvailabilityLocationController.php
+++ app/Http/Controllers/sdmobileapp/MilletAvailabilityLocationController.php
@@ -40,7 +40,7 @@ class MilletAvailabilityLocationController extends Controller
         {
             $obj = new \stdClass();
 
-            $obj->	description = '<b>'.$row->contact_name.'</b></b><br>'.$row->contact_phone.'<br><div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q='.urlencode($row->address).'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.emojilib.com">emojilib.com</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>';
+            $obj->	description = '<b>Contact:'.$row->contact_name.'</b></b><br>Phone:'.$row->contact_phone.'<br><div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q='.urlencode($row->address).'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.emojilib.com">emojilib.com</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>';
             $obj->	name = '<b>' .$row->	name . '</b>';
 
 
