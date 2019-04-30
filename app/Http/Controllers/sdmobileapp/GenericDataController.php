d760ed080973cfaff9781ed7f141b321eacb5dcb
Sudhi <sudhi@b2bsphere.com>
2019-04-29 19:39:54 -0500

    Simplified the disease tables.

--- app/Http/Controllers/sdmobileapp/GenericDataController.php
+++ app/Http/Controllers/sdmobileapp/GenericDataController.php
@@ -14,9 +14,48 @@ class GenericDataController extends Controller
         return response()->json(GenericData::where('type','=','recipe')->paginate(10));
     }
 
+    public static function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
     public function getMilletLocations()
     {
-        return response()->json(GenericData::where('type','=','contact')->paginate(10));
+        $rows = GenericData::where('type','=','contact')->paginate(15);
+
+
+        $rowsObj = (array) json_decode(json_encode($rows));
+
+
+        $arr = [];
+        $arr['current_page'] = $rowsObj['current_page'];
+        $arr['first_page_url'] = $rowsObj['first_page_url'];
+        $arr['next_page_url'] = $rowsObj['next_page_url'];
+        $arr['from']        = $rowsObj['from'];
+        $arr['last_page'] = $rowsObj['last_page'];
+        $arr['last_page_url'] = $rowsObj['last_page_url'];
+        $arr['per_page'] = $rowsObj['per_page'];
+        $arr['prev_page_url'] = $rowsObj['prev_page_url'];
+        $arr['to'] = $rowsObj['to'];
+        $arr['total'] = $rowsObj['total'];
+
+        foreach($rows as $row)
+        {
+            $obj = new \stdClass();
+
+            $description = str_replace("\r\n", "", $row->description);
+            if(self::isJson($description))
+            {
+                $contact = json_decode($description);
+                $obj->      description = '<b>Contact:</b>'.$contact->contact.'<br><b>Phone:</b>'.$contact->phone.'<br><div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q='.urlencode($contact->address).'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.emojilib.com">emojilib.com</a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>';
+            }
+            $obj->      name = '<b>' .$row->    name . '</b>';
+
+            $arr['data'][] =  $obj;
+
+        }
+
+        return response()->json($arr);
     }
 
 }
